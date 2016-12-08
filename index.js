2c23664ff55fa83b3c82b2d80eb6f029973c1545
evgeny <me@ch1p.com>
2016-12-08 16:58:42 +0100

    fix imports

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ var fs = require('fs'),
     path = require('path');
 
 ['server', 'client', 'Agents'].forEach(function(f) {
-  var exp = require(__dirname + '/lib/' + f),
+  var exp = require('./lib/' + f),
       keys = Object.keys(exp);
   for (var i = 0, len = keys.length; i < len; ++i)
     exports[keys[i]] = exp[keys[i]];
 });
 
-exports.auth = {};
-
-fs.readdirSync(__dirname + '/lib/auth').forEach(function(f) {
-  exports.auth[path.basename(f, '.js')] = require(__dirname + '/lib/auth/' + f);
-});
+exports.auth = {
+  None: require('./lib/auth/None'),
+  UserPassword: require('./lib/auth/UserPassword')
+};
